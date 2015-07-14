b3b5f19ba4221902d51b74ed4c99ac490ea8b823
Andrés Javier López <ajavier.lopez@gmail.com>
2015-07-14 08:20:56 -0600

    Shows a specific item in the catalog

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -23,3 +23,20 @@ def load_controllers(app):
         catalog = db_session.query(database.Item).all()
 
         return flask.render_template('catalog.html', catalog=catalog)
+
+    @app.route('/catalog/<int:item_id>')
+    def show_catalog_item(item_id):
+        '''Returns the html for a specific item in the catalog.
+
+        Looks for the item in the catalog with the id provided in the url. Then
+        returns the template with the item information.
+
+        Args:
+            item_id: id of the item that will be looked up in the catalog.
+        '''
+        db_session = database.get_session()
+        item = db_session.query(database.Item).get(item_id)
+        if item is not None:
+            return flask.render_template('item.html', item=item)
+        else:
+            flask.abort(404)
