203c975d6114378bafa8121450a1252928498fef
Andrés Javier López <ajavier.lopez@gmail.com>
2015-07-14 08:50:34 -0600

    Add categories to the catalog.
    Each item in the catalog must belong to one category.

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -4,36 +4,62 @@ import flask
 import database
 
 def load_controllers(app):
-    '''Defines the controllers for the catalog module.
+    """Defines the controllers for the catalog module.
 
     The controllers definitions have to be called explicitly, and don't run
     when the module is imported. This allow us to use the app decorators to
     define the routes.
-    '''
+    """
 
     @app.route('/')
     @app.route('/catalog')
     def show_catalog():
-        '''Returns the html with the full catalog.
+        """Returns the html with the full catalog sorted in categories.
 
         Read the database and obtain the item catalog, then loads it into the
         catalog template.
-        '''
+
+        Returns:
+            Html template with the list of categories.
+        """
         db_session = database.get_session()
-        catalog = db_session.query(database.Item).all()
+        catalog = db_session.query(database.Category).all()
 
-        return flask.render_template('catalog.html', catalog=catalog)
+        return flask.render_template('categories.html', catalog=catalog)
 
-    @app.route('/catalog/<int:item_id>')
-    def show_catalog_item(item_id):
-        '''Returns the html for a specific item in the catalog.
+    @app.route('/catalog/<string:category_name>')
+    def show_category_items(category_name):
+        """Returns the item list for a specific category.
+
+        Args:
+            category_name: The name of the category that is being listed.
+
+        Returns:
+            Html template with the list of items.
+        """
+        db_session = database.get_session()
+        category = db_session.query(database.Category).filter(
+                database.Category.name == category_name
+            ).one()
+
+        return flask.render_template('catalog.html', category=category,
+                                     catalog=category.items)
+
+    @app.route('/catalog/<string:category_name>/<int:item_id>')
+    def show_catalog_item(category_name, item_id):
+        """Returns the html for a specific item in the catalog.
 
         Looks for the item in the catalog with the id provided in the url. Then
         returns the template with the item information.
 
         Args:
-            item_id: id of the item that will be looked up in the catalog.
-        '''
+            category_name: Name of the current category. Its not used.
+            item_id: Id of the item that will be looked up in the catalog.
+
+        Returns:
+            Html with the item information.
+        """
+        _ = category_name
         db_session = database.get_session()
         item = db_session.query(database.Item).get(item_id)
         if item is not None:
--- vagrant/catalog/database.py
+++ vagrant/catalog/database.py
@@ -7,23 +7,34 @@ from sqlalchemy.ext import declarative
 Base = declarative.declarative_base()
 
 
+class Category(Base):
+    """Table that contains the catalog categories.
+    """
+
+    __tablename__ = 'categories'
+
+    id = sqla.Column(sqla.Integer, primary_key=True)
+    name = sqla.Column(sqla.String(100), nullable=False)
+    items = orm.relationship('Item', backref="category")
+
 class Item(Base):
-    '''Table that contains the items from the catalog.
+    """Table that contains the items from the catalog.
 
     Atrributes:
         id: primary key of the item
         name: name of the item
-    '''
+    """
 
     __tablename__ = 'catalog'
 
     id = sqla.Column(sqla.Integer, primary_key=True)
     name = sqla.Column(sqla.String(100), nullable=False)
+    category_id = sqla.Column(sqla.ForeignKey('categories.id'), nullable=False)
 
 
 def get_engine():
     '''Creates the engine that will be used with the database.'''
-    engine = sqla.create_engine('sqlite:///catalog.db')
+    engine = sqla.create_engine('sqlite:///catalog.sqlite')
     return engine
 
 
@@ -44,8 +55,12 @@ def fill_default_data():
     '''Adds default data to de database.'''
     db_session = get_session()
     default_items = ['red', 'blue', 'black', 'yellow', 'green']
+
+    new_category = Category(name='colors')
+    db_session.add(new_category)
+
     for item_name in default_items:
-        new_item = Item(name=item_name)
+        new_item = Item(name=item_name, category=new_category)
         db_session.add(new_item)
     db_session.commit()
 
