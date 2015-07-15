488212d6ee0d1a8fee135d755fd0e41660a7c9d5
Andrés Javier López <ajavier.lopez@gmail.com>
2015-07-15 10:41:52 -0600

    Add login with Google
    This is the basic login process with google. Login in still has no effect
    on the system.

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -3,9 +3,12 @@
 import flask
 
 import catalog
+import login
 
 if __name__ == '__main__':
     app = flask.Flask(__name__)
+    app.secret_key = 'those_are_my_secretz'
     app.debug = True
     catalog.load_controllers(app)
+    login.load_controllers(app)
     app.run(host='0.0.0.0', port=8000)
